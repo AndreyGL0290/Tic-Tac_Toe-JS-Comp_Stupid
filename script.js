d4f538d4bc582af6afe84bd79ced6f4e21572006
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2021-06-06 17:20:36 +0700

    First version

--- script.js
+++ script.js
@@ -1,162 +1,112 @@
-let figure = "крестик";
-let player_1_win = false;
-let player_2_win = false;
+let computer = false;
 let win_strike = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let button_restart;
-let context;
+let player_sym = 'X';
+let comp_sym = 'O';
 let canvas;
 let gap;
+let context;
+let ceil;
 
-// Корректируем размеры холста
-correction();
+// Корректируем длину и высоту холста
+correction(); 
 
-// Функция отрисовки
-function draw(number){
-    canvas = document.getElementById(number);
-    context = canvas.getContext("2d");
-    if (figure == "крестик" &&
-        player_1_win == false &&
-        player_2_win == false &&
-        // Если эта клеточка уже занята, то мы не сможем ее использовать
-        win_strike[win_strike.indexOf(number)] != undefined &&
-        win_strike[win_strike.indexOf(number)] != undefined){
-        // Рисует крестик
+// Определяем кто будет ходить первым
+if (Math.round(Math.random()) == 1){
+    computer = true;
+    comp_sym = 'X';
+    player_sym = 'O';
+    computerTurn();
+}
+
+function computerTurn(){
+    if (win_strike.indexOf(5) != -1){
+        ceil = 5;
+    }else{
+        while (win_strike.indexOf(ceil) == -1){
+            ceil = Math.random().toFixed(1) * 10;
+        }
+    }
+    canvas = document.getElementById(ceil);
+    context = canvas.getContext("2d")
+    context.beginPath();
+    context.lineWidth = 5;
+    if (comp_sym == 'X'){
         context.strokeStyle = '#ec4646';
         if (canvas.height == canvas.width){
-            context.beginPath();
             context.moveTo(10, 10)
             context.lineTo(canvas.width-10, canvas.height-10);
-            context.lineWidth = 5;
-            context.stroke();
             context.moveTo(canvas.width-10, 10);
             context.lineTo(10, canvas.height-10);
-            context.lineWidth = 5;
-            context.stroke();
         }
         else if (canvas.width > canvas.height){
-            context.beginPath();
             gap = (canvas.width - canvas.height) / 2 ;
             context.moveTo(gap+10, 10)
             context.lineTo(canvas.width-gap-10, canvas.height-10);
-            context.lineWidth = 5;
-            context.stroke();
             context.moveTo(canvas.width-gap-10, 10);
             context.lineTo(gap+10, canvas.height-10);
-            context.lineWidth = 5;
-            context.stroke();
         }
         else{
-            context.beginPath();
             gap = (canvas.height - canvas.width) / 2 ;
             context.moveTo(10, gap+10)
             context.lineTo(canvas.width-10, canvas.height-gap-10);
-            context.lineWidth = 5;
-            context.stroke();
             context.moveTo(10, canvas.height-gap-10);
             context.lineTo(canvas.width-10, gap+10);
-            context.lineWidth = 5;
-            context.stroke();
         }
-        figure = "нолик";
-        // Английская X
-        win_strike[win_strike.indexOf(number)] = 'X';
-        check("X", 1);
-        if (player_1_win){
-            document.getElementById('win').textContent = "Player №1 wins";
-            button_restart = document.getElementById('restart');
-            button_restart.textContent = "Restart";
-        }else{
-            standoff();
-        }
-    }else if (figure == "нолик" &&
-              player_1_win == false &&
-              player_2_win == false &&
-              // Если эта клеточка уже занята, то мы не сможем ее использовать
-              win_strike[win_strike.indexOf(number)] != undefined &&
-              win_strike[win_strike.indexOf(number)] != undefined){
-        // Рисует нолик
-        context.beginPath();
-        context.lineWidth = 5;
+    }else{
         context.strokeStyle = '#51c2d5';
         if (canvas.height >= canvas.width){
             context.arc(canvas.width / 2, canvas.height / 2, canvas.width / 2 - 10, 0, 2 * Math.PI);
         }else{
             context.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2 - 10, 0, 2 * Math.PI);
         }
-        context.stroke();
-        figure = "крестик";
-        // Английская O
-        win_strike[win_strike.indexOf(number)] = 'O';
-        check("O", 2);
-        if (player_2_win){
-            document.getElementById('win').textContent = "Player №2 wins";
-            button_restart = document.getElementById('restart');
-            button_restart.textContent = "Restart";
-        }
-        else{
-            standoff();
-        }
     }
+    win_strike[win_strike.indexOf(ceil)] = comp_sym;
+    context.stroke();
+    computer = false;
 }
 
-// Функция, отвечающая за победу того или иного игрока
-function check(symbol, player_number){
-    player_1_win = false;
-    player_2_win = false;
-
-    for (let i = 0; i<7; i += 3){
-        if (win_strike[i] == symbol && win_strike[i + 1] == symbol && win_strike[i + 2] == symbol){
-            if (player_number == 1){
-                player_1_win = true;
-            }else{
-                player_2_win = true;
-            }
-        }
-    }
-
-    for (let i = 0; i<3; i++){
-        if (win_strike[i] == symbol && win_strike[i + 3] == symbol && win_strike[i + 6] == symbol){
-            if (player_number == 1){
-                player_1_win = true;
-            }else{
-                player_2_win = true;
-            }
-        }
-    }
-
-    for (let i = 0; i<3; i += 3){
-        if (win_strike[i] == symbol && win_strike[i + 4] == symbol && win_strike[i + 8] == symbol){
-            if (player_number == 1){
-                player_1_win = true;
-            }else{
-                player_2_win = true;
-            }
-        }
-    }
-
-    for (let i = 0; i<3; i += 3){
-        if (win_strike[i + 2] == symbol && win_strike[i + 4] == symbol && win_strike[i + 6] == symbol){
-            if (player_number == 1){
-                player_1_win = true;
-            }else{
-                player_2_win = true;
-            }
-        }
-    }
-}
-
-function restart(){
-    for (let i = 1; i<10; i++){
-        canvas = document.getElementById(i);
+function playerTurn(number){
+    if (computer == false &&
+        // Если эта клеточка занята, то в нее нельзя ходить
+        win_strike.indexOf(number) != -1){
+        canvas = document.getElementById(number);
         context = canvas.getContext("2d");
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
+        context.lineWidth = 5;
+        if (player_sym == 'X'){
+            context.strokeStyle = '#ec4646';
+            if (canvas.height == canvas.width){
+                context.moveTo(10, 10)
+                context.lineTo(canvas.width-10, canvas.height-10);
+                context.moveTo(canvas.width-10, 10);
+                context.lineTo(10, canvas.height-10);
+            }
+            else if (canvas.width > canvas.height){
+                gap = (canvas.width - canvas.height) / 2 ;
+                context.moveTo(gap+10, 10)
+                context.lineTo(canvas.width-gap-10, canvas.height-10);
+                context.moveTo(canvas.width-gap-10, 10);
+                context.lineTo(gap+10, canvas.height-10);
+            }
+            else{
+                gap = (canvas.height - canvas.width) / 2 ;
+                context.moveTo(10, gap+10)
+                context.lineTo(canvas.width-10, canvas.height-gap-10);
+                context.moveTo(10, canvas.height-gap-10);
+                context.lineTo(canvas.width-10, gap+10);
+            }
+        }else{
+            context.strokeStyle = '#51c2d5';
+            if (canvas.height >= canvas.width){
+                context.arc(canvas.width / 2, canvas.height / 2, canvas.width / 2 - 10, 0, 2 * Math.PI);
+            }else{
+                context.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2 - 10, 0, 2 * Math.PI);
+            }
+        }
+        win_strike[win_strike.indexOf(number)] = [player_sym];
+        context.stroke();
+        computerTurn();
     }
-    document.getElementById('restart').textContent = '';
-    document.getElementById('win').textContent = '';
-    player_1_win = false;
-    player_2_win = false;
-    figure = 'крестик';
-    win_strike = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
 
 // Функция отвечающая за корректирование размеров холста
@@ -166,14 +116,4 @@ function correction(){
         canvas.width = canvas.clientWidth;
         canvas.height = canvas.clientHeight;
     }
-}
-
-function standoff(){
-    for (let i = 1; i<10; i++){
-        if (win_strike.indexOf(i) != -1){
-            return;
-        }
-    }
-    document.getElementById('win').textContent = "Draw";
-    document.getElementById('restart').textContent = "Restart";
 }
