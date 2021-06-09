b2db26418c9ee4124defab5348bb9d174a2186b4
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2021-06-09 16:11:31 +0700

    Little changes

--- script.js
+++ script.js
@@ -60,7 +60,6 @@ function computerTurn() {
     }
     win_strike[win_strike.indexOf(ceil)] = comp_sym;
     context.stroke();
-    // 
     if (check(comp_sym)){
         document.getElementById('win').textContent = "Computer wins";
         document.getElementById('restart').textContent = "Restart";
